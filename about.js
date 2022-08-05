5eb8cd4e1cc6bba12987d1b8b2b65c748d1fafa6
zachary sibson <zac.sibson@gmail.com>
2022-08-04 20:51:45 -0600

    added javascript

--- about.js
+++ about.js
@@ -5,10 +5,16 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	// console.log('form submit');
+	alert('You submitted the form!')
 }
 
+function imgHover(evt) {
+	alert('Wow, you cute!');
+}
 
 let form = document.querySelector('#contact');
+let imgAlert = document.querySelector('img');
 
 form.addEventListener('submit', handleSubmit);
+imgAlert.addEventListener('mouseover', imgHover);
